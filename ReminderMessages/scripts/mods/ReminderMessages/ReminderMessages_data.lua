9ba2bba09f399fc691af8b62017f6ed0943287c4
Backup158 <beekoop158@gmail.com>
2026-05-08 15:01:17 -0500

    fix: made unique ids

--- ReminderMessages/scripts/mods/ReminderMessages/ReminderMessages_data.lua
+++ ReminderMessages/scripts/mods/ReminderMessages/ReminderMessages_data.lua
@@ -12,34 +12,39 @@ local table_clone = table.clone
 mod:io_dofile("ReminderMessages/scripts/mods/ReminderMessages/Messages")
 
 local notifications_used = {}
-local message_notification_options = {
-	{
-		setting_id = "notify_immediately",
-		type = "checkbox",
-		default_value = false, 
-		sub_widgets = {
-			{
-				setting_id = "notify_time_interval",
-				type = "numeric",
-				range = {1, 69420},
-				unit_text = "notify_time_interval_units",
-				decimals_number = 0,
-				default_value = 300, -- default 5 mins 
-			},
-		},
-	},
-	{
-		setting_id = "notify_after_game",
-		type = "checkbox",
-		default_value = false, 
-	},
-}
 for message_option, _ in pairs(mod.messages) do
 	table.insert(notifications_used, {
 		setting_id = message_option,
 		type = "checkbox",
 		default_value = false, 
-		sub_widgets = table_clone(message_notification_options)
+		sub_widgets = {
+			{
+				setting_id = "notify_immediately_"..message_option,
+				title = "notify_immediately",
+				tooltip = "notify_immediately_description",
+				type = "checkbox",
+				default_value = false, 
+				sub_widgets = {
+					{
+						setting_id = "notify_time_interval_"..message_option,
+						title = "notify_time_interval",
+						tooltip = "notify_time_interval_description",
+						type = "numeric",
+						range = {1, 69420},
+						unit_text = "notify_time_interval_units",
+						decimals_number = 0,
+						default_value = 300, -- default 5 mins 
+					},
+				},
+			},
+			{
+				setting_id = "notify_after_game_"..message_option,
+				title = "notify_after_game",
+				tooltip = "notify_after_game_description",
+				type = "checkbox",
+				default_value = false, 
+			},
+		}
 	})
 end
 
