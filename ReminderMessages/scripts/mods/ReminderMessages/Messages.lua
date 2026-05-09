9c4e5be7cbe8e3308c36e93173fed00228eed2b4
Backup158 <beekoop158@gmail.com>
2026-05-09 16:58:34 -0500

    feat: HERESY

--- ReminderMessages/scripts/mods/ReminderMessages/Messages.lua
+++ ReminderMessages/scripts/mods/ReminderMessages/Messages.lua
@@ -8,7 +8,7 @@ mod.messages = {
 			"Reminder to drink your water!",
 			"Stay hydrated <3",
 			"Drink water, you beautiful dehydrated varlet <3",
-			"Drink water; take care of yourself.",
+			"Drink water| take care of yourself.",
 			"Pre-servitors require regular fluid intake for improved longevity.",
 			"DRINK DRINK DRINK",
 		},
@@ -23,4 +23,20 @@ mod.messages = {
 			"dry or dirty, fill my glass",
 		},
 	},
+	heresy = {
+		en = {
+			"I reject the false promises of the carrion god. I shall reap a terrible bounty from the death that I sow in your name, Father Nurgle. Pestilence and death to the weakling Imperium of Man!",
+			"Reject the lies of the False Emperor. Embrace your hunger, your lust, your desire. The universe is ours for the taking!",
+			"Destroy, for the sake of Destruction! Kill, for the sake of Killing!", -- ? | Codex: Chaos Space Marines (4th Edition)
+			"Death the False Emperor!", -- Sevatar, First Captain of the Night Lords | Codex: Chaos Space Marines (4th Edition)
+			"The minds of gods are not for mortals to know or to judge. Accept that Tzeentch has a place for all of us in his grand scheme, and be happy in the part you have to play.", -- Magnus the Red | Codex: Chaos Space Marines (4th Edition)
+			"In the embrace of the great Nurgle, I am no longer afraid, for with His pestilential favour I have become that which I once most feared: Death.", -- Kulvain Hestarius of the Death Guard | Codex: Chaos Space Marines (4th Edition)
+			"The Imperium is a weak old man, ready and waiting to be broken apart.", -- Lufgt Huron, The Tyrant of Badab | Codex: Chaos Space Marines (6th Edition)
+			"Cast down the idols! Destroy the temples! Slay the priests! Show these fools that they worship nothing more than a rotting corpse!", -- Dark Apostle Harzhan of the Word Bearers| Codex: Chaos Space Marines (4th Edition)
+			"I shall reap a terrible bounty from the death that I sow in your name, Father Nurgle...", -- Typhus the Traveler, Herald of Nurgle | Codex: Chaos Space Marines (6th Edition)
+			"There, feel the glory of necrosis, and rejoice! Nurgle loves you!", -- Ulkair, Great Unclean One | Warhammer 40,000: Dawn of War II - Retribution
+			"Sickness, disease, plague and pox, suffering and the slow, living rot. Such wondrous gifts does Nurgle seek to bestow upon the unworthy human cattle of the Imperium. We are merely the vectors by which his virulent beneficence may be spread to the undeserving masses.", -- Urgloth Rotheart, Plague Champion of the Death Guard | Dark Imperium, Death Guard booklet
+			"Let no good deed go unpunished. Let no evil deed go unrewarded.", -- Codex: Chaos Space Marines (4th Edition)
+		},
+	}
 }
