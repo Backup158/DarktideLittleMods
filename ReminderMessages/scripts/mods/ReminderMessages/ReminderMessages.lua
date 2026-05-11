1698dedadb9738b0ff5d483dff019ee565c7e384
Backup158 <beekoop158@gmail.com>
2026-05-10 20:46:27 -0500

    fix: on death not after death

--- ReminderMessages/scripts/mods/ReminderMessages/ReminderMessages.lua
+++ ReminderMessages/scripts/mods/ReminderMessages/ReminderMessages.lua
@@ -56,7 +56,7 @@ local function refresh_settings_cache()
         settings_messages[reminder_name].notify_immediately_timer = mod:get("notify_time_interval_"..reminder_name)
         settings_messages[reminder_name].after_win = mod:get("notify_after_win_"..reminder_name)
         settings_messages[reminder_name].after_loss = mod:get("notify_after_loss_"..reminder_name)
-        settings_messages[reminder_name].after_death = mod:get("notify_after_death_"..reminder_name)
+        settings_messages[reminder_name].on_death = mod:get("notify_on_death_"..reminder_name)
         settings_messages[reminder_name].on_load_screen = mod:get("notify_on_load_screen_"..reminder_name)
     end
 end
