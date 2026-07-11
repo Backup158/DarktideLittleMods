839dfc11e492b01f3d91db39076388d16c3b3572
Backup158 <beekoop158@gmail.com>
2026-07-11 02:11:47 -0500

    chore: simple mod name from template

--- TemplateMod/scripts/mods/TemplateMod/TemplateMod_localization.lua
+++ TemplateMod/scripts/mods/TemplateMod/TemplateMod_localization.lua
@@ -2,10 +2,10 @@ local mod = get_mod("TemplateMod")
 
 return {
 	mod_name = {
-		en = "Reminder Messages",
+		en = "TemplateMod",
 	},
 	mod_description = {
-		en = "Displays various messages after matches.",
+		en = "",
 	},
 	-- Version logging message appears after the version number, such as: v3.4.2 loaded uwu nya :3
 	mod_version_logging_message = {
