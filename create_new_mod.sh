520b1e34aeedb2fd755ac6a51b940d68946bce4b
Backup158 <beekoop158@gmail.com>
2026-07-11 01:44:18 -0500

    fix: renames folders and files properly

--- create_new_mod.sh
+++ create_new_mod.sh
@@ -21,9 +21,9 @@ cp -r "./${template_name}" "./${mod_name}"
 #   Finds all files recursively, then replaces the template name with the mod name. The {} ";" is for -exec to have no placeholder and a ; delimiter
 cd "${mod_name}"
 
-rename "s/${template_name}/${mod_name}/g" *
+find * -execdir rename "s/${template_name}/${mod_name}/g" "{}" \+
 echo "Renamed mod file names"
 # Replaces name instance in mod files
-find -name "${mod_name}*\.*" -exec sed -i "s/${template_name}/${mod_name}/g" {} ";"
+find -name "${mod_name}*\.*" -execdir sed -i "s/${template_name}/${mod_name}/g" "{}" \+
 #sed -i "s/${template_name}/${mod_name}/g" *
 echo "Replaced contents of mod files"
