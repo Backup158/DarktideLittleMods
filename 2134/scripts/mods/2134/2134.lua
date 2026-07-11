740680881fc1a05461fb6a84dda6f1d4b6cdc28a
Backup158 <beekoop158@gmail.com>
2026-07-11 02:07:41 -0500

    feat: basic setup for jp2

--- 2134/scripts/mods/2134/2134.lua
+++ 2134/scripts/mods/2134/2134.lua
@@ -2,13 +2,37 @@ local mod = get_mod("2134")
 mod.version = "1.0.0"
 
 -- #############################
--- Requirements
+-- Data
 -- #############################
+-- ###############
+-- Requirements and Performance
+-- ###############
+
+-- ###############
+-- Mod Locals
+-- ###############
+local simple_audio
+local simple_audio_random
 
 -- #############################
 -- Helper Functions
 -- #############################
+function mod.crash_game(play_id)
+    -- Triggering a deadlock
+    for i = 1, 9999 do
+        mod:echo("HAHAHAHHAHAHAHAH")
+    end
+    error(mod:localize("jan_pawel_is_dead"))
+end
 
+
+local function play_barka()
+    simple_audio.play_file("barka.mp3", {
+        audio_type = "sfx",
+        volume = mod_option_volume or 100,
+        on_finished = crash_game
+    })
+end
 
 -- #########################################
 -- Hooks
--- 2134/scripts/mods/2134/2134_localization.lua
+++ 2134/scripts/mods/2134/2134_localization.lua
@@ -1,16 +1,17 @@
-local mod = get_mod("2134")
-
-
+local mod = get_mod("2137")
 
 return {
 	mod_name = {
-		en = "2134",
+		en = "2137",
 	},
 	mod_description = {
-		en = "Displays various messages after matches.",
+		en = "jan pawel xddd",
 	},
 	-- Version logging message appears after the version number, such as: v3.4.2 loaded uwu nya :3
 	mod_version_logging_message = {
 		en = " loaded uwu nya :3",
+	},
+	jan_pawel_is_dead = {
+		en = "Osobisty lekarz papieski Renato Buzzonetti stwierdził śmierć papieża Jana Pawła II o godzinie 21:37 czasu miejscowego",
 	},
 }
