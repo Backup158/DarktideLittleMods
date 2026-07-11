e73fb307d601a6c3ba8aeee4ddef4a8d4462f5c1
Backup158 <beekoop158@gmail.com>
2026-07-11 01:47:28 -0500

    fix: exit not return

--- create_new_mod.sh
+++ create_new_mod.sh
@@ -8,22 +8,22 @@ else
     mod_name=$1
 fi
 
-# Checks if mod name exists already
+# Checks if mod name exists already. Stop if so
 if [ -d "./${mod_name}" ]; then 
     echo "Mod name already in use."
-    return
+    exit 1
 fi
 
 # Copies template
 cp -r "./${template_name}" "./${mod_name}"
 
-# Renames files
-#   Finds all files recursively, then replaces the template name with the mod name. The {} ";" is for -exec to have no placeholder and a ; delimiter
 cd "${mod_name}"
 
+# Renames files
+#   Finds all files recursively, then replaces the template name with the mod name. The {} ";" is for -exec to have no placeholder and a ; delimiter
 find * -execdir rename "s/${template_name}/${mod_name}/g" "{}" \+
 echo "Renamed mod file names"
+
 # Replaces name instance in mod files
 find -name "${mod_name}*\.*" -execdir sed -i "s/${template_name}/${mod_name}/g" "{}" \+
-#sed -i "s/${template_name}/${mod_name}/g" *
 echo "Replaced contents of mod files"
